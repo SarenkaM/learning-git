a12ed4cee7e9ef73e3f3bdb6a9f92db488165eb2
Marcin Sarna <sarna077@gmail.com>
2023-01-25 21:54:47 +0100

    Add Init_project.py

--- Init_project.py
+++ Init_project.py
@@ -1,22 +1,12 @@
 lista_zakupów = {"piekarnia": ["chleb", "pączek", "bułki",],
                 "warzywniak": ["marchew", "seler", "rukola",]}
 
-
 for sklep, rzeczy in lista_zakupów.items():
     rzeczy = [i.capitalize() for i in rzeczy]
-    print(f"idę do {sklep.capitalize()}, kupuję tu następujące rzeczy: {rzeczy}")
+    print(f"idę do {sklep.capitalize()}, kupuję: {rzeczy}")
 
-
-
-sum = 0
+    sum = 0
 for i in lista_zakupów.values():
-    for z in i:
-        sum = sum + 1
-
+    sum = len(i)
+    sum = sum + sum
 print(f"W sumie kupuję {sum} produktów")
-
-list = []
-
-for i in range(101):
-    if i % 5 == 0:
-        list.append(i)
