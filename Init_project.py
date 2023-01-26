4c8f7a297a4692a3abfb427d04672dd7426b96ce
Marcin Sarna <sarna077@gmail.com>
2023-01-26 21:42:34 +0100

    Remove all, add regards

--- Init_project.py
+++ Init_project.py
@@ -1,19 +1,5 @@
-lista_zakupów = {"piekarnia": ["chleb", "pączek", "bułki",],
-                "warzywniak": ["marchew", "seler", "rukola",]}
+print("Hej Maciek, życzę Ci miłego wieczoru")
 
-for sklep, rzeczy in lista_zakupów.items():
-    rzeczy = [i.capitalize() for i in rzeczy]
-    print(f"idę do {sklep.capitalize()}, kupuję: {rzeczy}")
 
-    sum = 0
-for i in lista_zakupów.values():
-    sum = len(i)
-    sum = sum + sum
-print(f"W sumie kupuję {sum} produktów produktów, zjem je wszystkie")
-
-print("Na pewno będą dobre")
-
-print("godzinę później")
-print("Zjedłem wszystko, boli mnie brzuch")
 
 
